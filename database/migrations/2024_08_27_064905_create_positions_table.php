530489749c275ba8b483d1e83ae67ac9b3232537
Ironsick_ <daryllfortunado@gmail.com>
2024-08-27 14:52:48 +0800

    chore: Add unique name field and nullable report_to foreign key to positions table migration

--- database/migrations/2024_08_27_064905_create_positions_table.php
+++ database/migrations/2024_08_27_064905_create_positions_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('positions', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->foreignId('report_to')->nullable()->constrained('positions')->nullOnDelete();
             $table->timestamps();
         });
     }
