e0f6d03330e0eb1e52d1ff1eed8c560b10738850
Ironsick_ <daryllfortunado@gmail.com>
2024-08-27 14:54:22 +0800

    feat: Add API routes and controller for managing positions

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PositionController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('positions')->group(function () {
+    Route::post('/', [PositionController::class, 'store']);
+    Route::get('/', [PositionController::class, 'index']);
+    Route::get('/{id}', [PositionController::class, 'show']);
+    Route::put('/{id}', [PositionController::class, 'update']);
+    Route::delete('/{id}', [PositionController::class, 'destroy']);
 });
