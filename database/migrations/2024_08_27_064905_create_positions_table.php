ed9a1c56fe2513031e14ed1d0ab7a9e280fdbeee
Ironsick_ <daryllfortunado@gmail.com>
2024-08-27 22:31:54 +0800

    refactor: Update positions table migration to add nullable foreign key constraint for report_to field

--- database/migrations/2024_08_27_064905_create_positions_table.php
+++ database/migrations/2024_08_27_064905_create_positions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('positions', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->foreignId('report_to')->nullable()->constrained('positions')->nullOnDelete();
+            $table->foreignId('report_to')->nullable()->constrained('positions', 'id')->nullOnDelete();
             $table->timestamps();
         });
     }
