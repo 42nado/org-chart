a8568a05beb2bc00b4e7a87695b1d86104900b44
Ironsick_ <daryllfortunado@gmail.com>
2024-08-28 08:40:59 +0800

    refactor: Update welcome.blade.php add name to form and update bootstrap cdn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,25 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Organizational Chart Management</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>Organizational Chart</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 </head>
 
 <body>
     <div class="container mt-5">
-        <h2>Organizational Chart Management</h2>
+        <h2>Organizational Chart</h2>
 
         <!-- Form to Add/Update Position -->
         <form id="positionForm">
             <div class="form-group">
                 <label for="name">Position Name:</label>
-                <input type="text" class="form-control" id="name" placeholder="Enter position name" required>
+                <input type="text" class="form-control" name="name" id="name"
+                    placeholder="Enter position name" required>
             </div>
             <div class="form-group">
                 <label for="report_to">Reports To:</label>
-                <select class="form-control" id="report_to">
+                <select class="form-control" name="report_to" id="report_to">
                     <option value="">Select a position</option>
                 </select>
             </div>
@@ -75,7 +79,7 @@
                             <tr>
                                 <td>${position.id}</td>
                                 <td>${position.name}</td>
-                                <td>${position.parent_position ? position.parent_position.name : 'None'}</td>
+                                <td>${position.parent_position ? position.parent_position.name : ''}</td>
                                 <td>
                                     <button class="btn btn-warning btn-sm" onclick="editPosition(${position.id})">Edit</button>
                                     <button class="btn btn-danger btn-sm" onclick="deletePosition(${position.id})">Delete</button>
